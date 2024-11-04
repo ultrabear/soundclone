d12202034be40cf456e2feac5cac1be6f5871023
ultrabear <bearodark@gmail.com>
2024-11-04 15:38:27 -0800

    remove vites boilerplate

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+//@ts-expect-error no typestubs
+import eslint from "vite-plugin-eslint";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => ({
+	plugins: [
+		react(),
+		eslint({
+			lintOnStart: true,
+			failOnError: mode === "production",
+		}),
+	],
+	server: { proxy: { "/api": "http://localhost:5000" } },
+}));

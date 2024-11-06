48245c89884523de0cdd7c00a79307bb78bf6809
Joe Corcoran <joecorcoran.com@gmail.com>
2024-11-06 14:15:24 -0500

    Rebase to remove api.py from Models draft commit

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,13 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
-
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from datetime import date
 
 
 class Base(DeclarativeBase):
     __slots__ = ()
-
 
 db = SQLAlchemy(model_class=Base)
 
@@ -17,21 +15,40 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
     username: Mapped[str] = mapped_column(nullable=False)
-    artist_meta_id: Mapped[int] = mapped_column(
-        ForeignKey("artist_meta.id"), nullable=True, unique=True
-    )
-    artist_meta: Mapped["ArtistMeta"] = relationship(back_populates="parent")
-    profile_image: Mapped[str] = mapped_column(nullable=True)
+    profile_image: Mapped[str | None] = mapped_column(nullable=True)
+    stage_name: Mapped[str | None] = mapped_column(nullable=True)
+    first_release: Mapped[date | None] = mapped_column(nullable=True)
+    biography: Mapped[str | None] = mapped_column(nullable=True)
+    location: Mapped[str | None] = mapped_column(nullable=True)
+    homepage: Mapped[str | None] = mapped_column(nullable=True)
 
 
-class ArtistMeta(Base):
-    __tablename__ = "artist_meta"
+class Song(Base):
+    __tablename__ = "songs"
 
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
-    stage_name: Mapped[str] = mapped_column(nullable=True)
-    first_release: Mapped[date] = mapped_column(nullable=True)
-    biography: Mapped[str] = mapped_column(nullable=True)
-    location: Mapped[str] = mapped_column(nullable=True)
-    homepage: Mapped[str] = mapped_column(nullable=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    artist_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    genre: Mapped[str | None] = mapped_column(nullable=True)
+    thumb_url: Mapped[str | None] = mapped_column(nullable=True)
+    song_ref: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[date] = mapped_column(nullable=False)
+    updated_at: Mapped[date] = mapped_column(nullable=False)
 
-    parent: Mapped[User] = relationship(back_populates="artist_meta")
+class Playlist(Base):
+    __tablename__ = "playlists"
+
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    thumbnail: Mapped[str | None] = mapped_column(nullable=True)
+    created_at: Mapped[date] = mapped_column(nullable=False)
+    updated_at: Mapped[date] = mapped_column(nullable=False)
+
+class PlaylistsJoin(Base):
+    __tablename__ = "playlists_join"
+
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    playlist_id: Mapped[int] = mapped_column(ForeignKey("playlists.id"), nullable=False)
+    song_id: Mapped[int] = mapped_column(ForeignKey("songs.id"), nullable=False)
+

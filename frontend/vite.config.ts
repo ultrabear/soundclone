8507ffb59a7812aaeef03d447cca23920f543c0a
joemcdonough15 <joemcdonoughmusic@gmail.com>
2024-11-08 19:26:35 -0500

    fix formatting on vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,12 +5,12 @@ import eslint from "vite-plugin-eslint";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    eslint({
-      lintOnStart: true,
-      failOnError: mode === "production",
-    }),
-  ],
-  server: { proxy: { "/api": "http://127.0.0.1:5000" } },
+	plugins: [
+		react(),
+		eslint({
+			lintOnStart: true,
+			failOnError: mode === "production",
+		}),
+	],
+	server: { proxy: { "/api": "http://127.0.0.1:5000" } },
 }));

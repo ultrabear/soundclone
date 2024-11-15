a3add589c05c9bab0ad33e0812d20e2c37bd378b
joemcdonough15 <joemcdonoughmusic@gmail.com>
2024-11-15 14:08:23 -0500

    integrate aws into create playlist route

--- backend/src/api/playlist_routes.py
+++ backend/src/api/playlist_routes.py
@@ -20,7 +20,8 @@ from ..backend_api import (
     Created,
 )
 from .aws_integration import DEFAULT_THUMBNAIL_IMAGE
-from .song_routes import delete_resource_from_aws
+from .song_routes import create_resource_on_aws, delete_resource_from_aws
+from ..forms.playlist_form import PlaylistForm
 
 playlist_routes = Blueprint("playlists", __name__, url_prefix="/api/playlists")
 
@@ -47,30 +48,39 @@ def db_song_to_api_song(song: Song) -> GetSong:
 @playlist_routes.route("", methods=["POST"])
 @login_required
 def create_playlist() -> Union[Created[IdAndTimestamps], Tuple[ApiError, int]]:
-    data: BasePlaylist = request.get_json()
-    name = data.get("name")
-    thumbnail = data.get("thumbnail")
+    form = PlaylistForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
 
-    if not name:
-        return ApiError(message="Name is required", errors={"name": "Playlist name cannot be empty"}), 400
+    if form.validate_on_submit():
+        # handle name is required
+        if not form.data["name"]:
+            return ApiError(message="Name is required", errors={"name": "Playlist name cannot be empty"}), 400
 
-    playlist = Playlist(
-        name=name,
-        user_id=current_user.id,
-        thumbnail=thumbnail,
-        created_at=datetime.now(timezone.utc),
-        updated_at=datetime.now(timezone.utc),
-    )
+        # handle thumbnail default or user provided
+        thumbnail_url = DEFAULT_THUMBNAIL_IMAGE
+        if form.data["thumbnail_img"] is not None:
+            thumbnail_url = create_resource_on_aws(form.data["thumbnail_img"], "image")
 
-    db.session.add(playlist)
-    db.session.commit()
+        # construct the playlist
+        new_playlist = Playlist(
+            name=form.data["name"],
+            user_id=current_user.id,
+            thumbnail=thumbnail_url,
+            created_at=datetime.now(timezone.utc),
+            updated_at=datetime.now(timezone.utc),
+        )
 
-    response: IdAndTimestamps = {
-        "id": playlist.id,
-        "created_at": str(playlist.created_at),
-        "updated_at": str(playlist.updated_at),
-    }
-    return response, 201
+        db.session.add(new_playlist)
+        db.session.commit()
+
+        response: IdAndTimestamps = {
+            "id": new_playlist.id,
+            "created_at": str(new_playlist.created_at),
+            "updated_at": str(new_playlist.updated_at),
+        }
+        return response, 201
+
+    return form.errors, 400
 
 
 # 2. Update a playlist (name can be changed)

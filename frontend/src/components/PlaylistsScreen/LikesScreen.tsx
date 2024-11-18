894cf0a4a8e745e1665db8feb098f5604d953d4f
ultrabear <bearodark@gmail.com>
2024-11-17 18:42:55 -0800

    fix likes page

--- frontend/src/components/PlaylistsScreen/LikesScreen.tsx
+++ frontend/src/components/PlaylistsScreen/LikesScreen.tsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
-import { useAppDispatch } from "../../store";
+import { useAppDispatch, useAppSelector } from "../../store";
 import { getLikes } from "../../store/slices/songsSlice";
-import { PlaylistTile } from "./PlaylistTile";
+import { createSelector } from "@reduxjs/toolkit";
+import type { RootState } from "../../store";
+import { SongListItem } from "../SongListItem";
+
+const selectLikes = createSelector(
+	(state: RootState) => state.session.likes,
+	(likes) => Object.keys(likes).map(Number),
+);
 
 function LikesScreen() {
 	const dispatch = useAppDispatch();
 	const [loaded, setLoaded] = useState<boolean>(false);
+
+	const likes = useAppSelector(selectLikes);
 
 	if (!loaded) {
 		setLoaded(true);
@@ -13,11 +22,7 @@ function LikesScreen() {
 		dispatch(getLikes());
 	}
 
-	return (
-		<div className="playlists-screen">
-			<PlaylistTile id="likes" />
-		</div>
-	);
+	return likes.map((s, i) => <SongListItem key={s} index={i} songId={s} />);
 }
 
 export default LikesScreen;
